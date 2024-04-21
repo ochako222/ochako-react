f13cf8ac38035d469bf1a03c030523d6bc1c06d8
Oleksandr Chako <a.chako2012@gmail.com>
2024-04-21 14:55:28 +0200

    WIP

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -3,13 +3,13 @@ import { getDatabase } from 'firebase/database';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyBNghR9OimZqPRsTCBuBwZaJCSOOKtjAR4',
-    authDomain: 'alex-pomidoro.firebaseapp.com',
-    databaseURL: 'https://alex-pomidoro-default-rtdb.europe-west1.firebasedatabase.app',
-    projectId: 'alex-pomidoro',
-    storageBucket: 'alex-pomidoro.appspot.com',
-    messagingSenderId: '752885',
-    appId: '1:752885762949:web:0d3c31a1301995112352ec'
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 let app = null;
